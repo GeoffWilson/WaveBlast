76063a0e1594c895f0505705dfc14f7997597f3b
Benshiro <bencarvell@gmail.com>
2015-01-01 20:08:01 +0000

    pig update

--- src/co/piglet/waveblast/GameEngine.java
+++ src/co/piglet/waveblast/GameEngine.java
@@ -593,7 +593,7 @@ public class GameEngine {
                     switch (enemy.enemyType) {
 
                         case TURRET_NORTH: {
-                            Sprite s2 = new Sprite("sprites/shot-e-2.png", cache);
+                            Sprite s2 = new Sprite("sprites/shot-e-3.png", cache);
                             s2.x = enemy.x;
                             s2.y = enemy.y + 25;
                             s2.setActiveAnimation("east", 1000);
@@ -603,7 +603,7 @@ public class GameEngine {
                         }
 
                         case TURRET_SOUTH: {
-                            Sprite s2 = new Sprite("sprites/shot-e-2.png", cache);
+                            Sprite s2 = new Sprite("sprites/shot-e-3.png", cache);
                             s2.x = enemy.x;
                             s2.y = enemy.y + 25;
                             s2.setActiveAnimation("east", 1000);
